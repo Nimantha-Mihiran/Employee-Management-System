49a221b0b78ba30a7ab8974f49a954bf1a4081d8
Nimantha-Mihiran <nimanthamihiran14@gmail.com>
2025-02-21 12:17:23 +0530

    add modelmapper

--- src/main/java/com/example/employeems/service/EmployeeService.java
+++ src/main/java/com/example/employeems/service/EmployeeService.java
@@ -3,6 +3,7 @@ package com.example.employeems.service;
 import com.example.employeems.dto.EmployeeDTO;
 import com.example.employeems.entity.Employee;
 import com.example.employeems.repo.EmployeeRepo;
+import com.example.employeems.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class EmployeeService {
